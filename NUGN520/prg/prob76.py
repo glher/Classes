453bc26bd35ed103e129694ae01399b433803fa5
glher <glher@mymail.mines.edu>
2017-03-07 11:06:47 -0700

    NUGN520: Starts HW7

---
+++ NUGN520/prg/prob76.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+m = 10    # size of system is m x m
+
+dx = 0.12
+deltax, k, qtriple, h = (dx/10)**2, 10., 2.e6, 100.    # BG units
+tb = 1000.
+ts = 600.
+dtg = (0.5*deltax)*qtriple/k
+bi = h * dx / k
+
+amat = np.zeros((m,m))
+b = np.zeros(m)
+    
+amat[0,0] = 1.
+amat[1,1] = 1.
+b[0] = tb
+b[1] = tb
+
+for i in range(2, m, 2):
+    try:
+        amat[i,i-2] = -0.25
+        amat[i,i] = 1.
+        amat[i,i+1] = -0.5
+        amat[i,i+2] = -0.25
+        b[i] = dtg/2.
+    except IndexError:
+        break
+
+for i in range(3, m, 2):
+    try:
+        amat[i,i-2] = -1/(2*bi+4)
+        amat[i,i-1] = -1/(bi+2)
+        amat[i,i] = 1.
+        amat[i,i+2] = -1/(2*bi+4)
+        b[i] = (bi*ts + dtg)/(bi+2)
+    except IndexError:
+        break
+
+amat[m-1,m-1] = 1.
+amat[m-1,m-2] = -1/(2*bi+2)
+amat[m-1,m-3] = -1/(2*bi+2)
+b[m-1] = (2*bi*ts + dtg)/(2*bi+2)
+
+
+amat[m-2,m-2] = 1.
+amat[m-2,m-1] = -1/(bi+2)
+amat[m-2,m-4] = -1/(bi+2)
+b[m-2] = (bi*ts + dtg)/(bi+2)
+print(b)
+ysol = np.linalg.solve(amat,b)
+
+print('Temperature solution:')
+
+temp = []
+for i in range(5):
+    for j in range(2):
+        temp.append("T(%s,%s)" % (i, j))
+        
+for i,t in enumerate(temp):
+    print("%s = %.1f" % (t, ysol[i]))
+    
+# check of solution: 
+if not np.allclose(np.dot(amat, ysol), b):
+    print("Solution does not match!")
+    
+
+    
